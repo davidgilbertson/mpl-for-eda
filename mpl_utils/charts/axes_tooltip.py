74d633f60347b10cbd222c51d3d0732788f9da2f
davidgilbertson <gilbertson.david@gmail.com>
2024-02-16 05:59:53 +1100

    Edits

--- mpl_utils/charts/axes_tooltip.py
+++ mpl_utils/charts/axes_tooltip.py
@@ -24,15 +24,15 @@ class add_axes_tooltip(mpl_utils.EventsMixin):
         ax : matplotlib.axes.Axes
             The main axes object on which the tooltip will be displayed.
         render : callable
-            A callback function that accepts three arguments: `ax`, `hover_ax`, and `event`.
-            This function is responsible for plotting data into `hover_ax` based on the mouse event.
+            A callback function that accepts three arguments: `ax`, `tooltip_ax`, and `event`.
+            This function is responsible for plotting data into `tooltip_ax` based on the mouse event.
             It should return `True` if the tooltip should be displayed, `False` otherwise.
         width : int, optional
-            The width of the hover tooltip in pixels. Default is 300.
+            The width of the tooltip axes in pixels. Default is 300.
         height : int, optional
-            The height of the hover tooltip in pixels. Default is 185.
+            The height of the tooltip axes in pixels. Default is 185.
         alpha : float, optional
-            The transparency of the hover tooltip. Default is 0.9.
+            The transparency of the tooltip axes. Default is 0.9.
         """
         super().__init__(ax)
         self.ax = ax
@@ -42,57 +42,55 @@ class add_axes_tooltip(mpl_utils.EventsMixin):
         self.height = height
         self.alpha = alpha
 
-        self.hover_ax = self.fig.add_axes(
+        self.tooltip_ax = self.fig.add_axes(
             rect=(0, 0, 1, 1),
             visible=False,
         )
 
-        self.hover_ax.set_facecolor(plt.rcParams["grid.color"])
-        self.hover_ax.spines[:].set(
-            visible=True,
-            color=plt.rcParams["text.color"],
-            linewidth=0.5,
-        )
+        self.tooltip_ax.set_facecolor(plt.rcParams["grid.color"])
+        self.tooltip_ax.spines[:].set_visible(True)
+        self.tooltip_ax.spines[:].set_color(plt.rcParams["text.color"])
+        self.tooltip_ax.spines[:].set_linewidth(0.5)
 
-        self.fig.canvas.mpl_connect("motion_notify_event", self.on_hover)
+        self.fig.canvas.mpl_connect("motion_notify_event", self.on_mouse_move)
         ax._add_axes_tooltip_ref = self
 
-    def on_hover(self, event: MouseEvent):
+    def on_mouse_move(self, event: MouseEvent):
         if event.inaxes != self.ax:
             return
 
-        self.hover_ax.clear()
-        self.hover_ax.patch.set_alpha(self.alpha)
-        show_tooltip = self.render(self.ax, self.hover_ax, event)
+        self.tooltip_ax.clear()
+        self.tooltip_ax.patch.set_alpha(self.alpha)
+
+        show_tooltip = self.render(self.ax, self.tooltip_ax, event)
 
         if show_tooltip:
-            self.show_hover_ax(event)
+            self.show_tooltip_ax(event)
         else:
-            self.hide_hover_ax()
+            self.hide_tooltip_ax()
 
-    def on_leave(self, event):
-        self.hide_hover_ax()
-
-    def show_hover_ax(self, event):
-        pad_px = 15
+    def show_tooltip_ax(self, event):
         is_left = event.x < self.fig.bbox.width / 2
         is_bottom = event.y < self.fig.bbox.height / 2
 
         bb_px = Bbox.from_bounds(
-            x0=event.x + (pad_px if is_left else -self.width - pad_px),
-            y0=event.y + (pad_px if is_bottom else -self.height - pad_px),
+            x0=event.x + (15 if is_left else -self.width - 15),
+            y0=event.y + (15 if is_bottom else -self.height - 15),
             width=self.width,
             height=self.height,
         )
 
-        self.hover_ax.set(
+        self.tooltip_ax.set(
             position=bb_px.transformed(self.fig.transFigure.inverted()),
             visible=True,
         )
         self.fig.canvas.draw_idle()
 
-    def hide_hover_ax(self):
-        self.hover_ax.set_visible(False)
+    def on_leave(self, event):
+        self.hide_tooltip_ax()
+
+    def hide_tooltip_ax(self):
+        self.tooltip_ax.set_visible(False)
 
         self.fig.canvas.draw_idle()
 
@@ -100,7 +98,7 @@ class add_axes_tooltip(mpl_utils.EventsMixin):
 if __name__ == "__main__":
     df = pd.read_csv("../../data/crop-data.csv")
 
-    def render_ax(ax: Axes, item):
+    def render_heatmap_ax(ax: Axes, item: tuple[str, pd.DataFrame]):
         item_name, item_df = item
         chart_df = item_df.pivot_table(
             index="Country",
@@ -117,27 +115,30 @@ if __name__ == "__main__":
 
     paginator = mpl_utils.plot_paginated(
         items=df.groupby("Region"),
-        render=render_ax,
+        render=render_heatmap_ax,
         items_per_page=1,
     )
 
-    def render_hover_ax(heatmap_ax: Axes, hover_ax: Axes, event):
+    def render_tooltip_ax(
+        heatmap_ax: Axes,
+        tooltip_ax: Axes,
+        event: MouseEvent,
+    ) -> bool:
         crop = heatmap_ax.format_xdata(event.xdata)
         country = heatmap_ax.format_ydata(event.ydata)
 
         chart_df = df[df.Crop.eq(crop) & df.Country.eq(country)]
-        hover_ax.plot(chart_df.Year, chart_df.Yield)
-        hover_ax.set(xticks=[], yticks=[])
-        hover_ax.set_ylim(0)
 
         if chart_df.Yield.dropna().empty:
-            show_tooltip = False
-        else:
-            show_tooltip = True
+            return False
 
-        return show_tooltip
+        tooltip_ax.plot(chart_df.Year, chart_df.Yield)
+        tooltip_ax.set(xticks=[], yticks=[])
+        tooltip_ax.set_ylim(0)
+
+        return True
 
     add_axes_tooltip(
         ax=paginator.axs[0],
-        render=render_hover_ax,
+        render=render_tooltip_ax,
     )
